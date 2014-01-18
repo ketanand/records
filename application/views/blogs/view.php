a6b7f3fc03792be92125a9862108d22071449404
Ketan <ketan@ketan.(none)>
2014-01-18 14:32:52 +0530

    added view for blog

---
+++ application/views/blogs/view.php
@@ -0,0 +1,270 @@
+<!-- #banner -->
+
+<header class="section-header">
+<h1><span><?echo strtoupper("BLOGS");?></span></h1>
+<span class="sub-heading"><?php echo strtoupper("Music is what we preach, what we eat and what we read.");?></span>
+   <div class="social-icons">
+	<a href="#" class="social24 facebook"></a>
+	<a href="#" class="social24 twitter"></a>
+	<!--a href="#" class="social24 pinterest">Pinterest</a>
+	<a href="#" class="social24 linkedin">LinkedIn</a>
+	<a href="#" class="social24 stumble">Stumble</a>
+	<a href="#" class="social24 dribbble">Dribbble</a>
+	<a href="#" class="social24 digg">Digg</a-->
+	<a href="#" class="social24 google"></a>
+	<!--a href="#" class="social24 delicious">Delicious</a>
+	<a href="#" class="social24 youtube">Youtube</a>
+	<a href="#" class="social24 instagram">Instagram</a>
+	<a href="#" class="social24 vimeo">Vimeo</a-->
+    </div>
+</header>
+<div id="main-content">
+            <div id="articles-content classic-layout entries" class="articles-content classic-layout">
+		<header>
+			<div class="summary row collapse">
+			      <h3 class="title" itemprop="title headline">This Artist Has Added The Missing Truth To Gorgeous Ads For Luxury Products</h3>
+      		        	<span class="deck pull-three" itemprop="deck description"><p>Depictions of fancy lifestyles rarely include the workers who keep the swimming pools clean and the houses sparkling.</p></span>
+       			</div>
+	    <div class="social-icons">
+		<a href="#" class="social24 facebook"></a>
+		<a href="#" class="social24 twitter"></a>
+		<!--a href="#" class="social24 pinterest">Pinterest</a>
+		<a href="#" class="social24 linkedin">LinkedIn</a>
+		<a href="#" class="social24 stumble">Stumble</a>
+		<a href="#" class="social24 dribbble">Dribbble</a>
+		<a href="#" class="social24 digg">Digg</a-->
+		<a href="#" class="social24 google"></a>
+		<!--a href="#" class="social24 delicious">Delicious</a>
+		<a href="#" class="social24 youtube">Youtube</a>
+		<a href="#" class="social24 instagram">Instagram</a>
+		<a href="#" class="social24 vimeo">Vimeo</a-->
+    	    </div>
+	  </header>
+	  <article class="post-1569 post type-post status-publish format-standard hentry category-articles category-interviews tag-amirtharaj-stephen tag-bhumika-popli tag-interview grid-4">
+		<div class="row collapse">
+    <div class="node-article articles-content classic-layout" itemprop="articleBody">
+      <p>Imagine a magazine spread for a modernist, beachfront home. Now, try picturing that photograph with the workers who keep that home's pool clean. Anything feel different? Try another: How might the appeal of an ad highlighting a living room’s accent furniture differ if it showed the woman who dusted those pieces every day for $7 an hour?</p>
+
+<p>“It’s a very humiliating thing to feel like you’re not as important as a luxury item,” 27-year-old artist <a href="http://ramirogomezjr.blogspot.com/" target="_blank">Ramiro Gomez</a> tells me over the phone. We’re talking about his "magazine paintings" depicting these arrangements, along with the arrangement Gomez himself worked for two years, as a live-in nanny for a wealthy West Hollywood family. Among his other duties, like childcare and cleaning, he remembers signing in FedEx packages containing expensive Louis Vuitton purses at the door.</p>
+
+<figure class="inline-large inline">
+<img src="//e.fastcompany.net/multisite_files/fastcompany/imagecache/inline-large/inline/2014/01/3024333-inline-cirilo-with-a-leaf-blower.jpg" alt="">
+</figure>
+
+<p>Gomez, who grew up in a hard-working, working class Mexican immigrant family, says that he considered the job a blessing after the grandmother who helped raise him suddenly passed away, and he found himself jobless and grieving. And yet, signing in those luxury items felt deeply off. “It was interesting the feeling that would happen as I was signing off this purse, that the family had so much already, yet they weren’t able to pay [me] more,” Gomez says. “I took it personally, in a way.”</p>
+
+<p>That feeling of degrading invisibility carries through his work, in a series on display at the Charles James gallery in Los Angeles' Chinatown this month. The paintings use ads torn from luxury magazines as canvases, but also feature a character absent from, but critically important to, the aspirational universe set in 8 by 11 inches of paper gloss: America’s domestic workforce.</p>
+
+<p>Gomez started making these images when he was still employed as a nanny through 2011, fishing magazines like <em>Dwell</em> and <em>Luxe</em> out of the trash, then tearing the ads out as an act of creative catharsis. On a display of a Caucasian family standing in front of a row of neatly trimmed hedges before their backyard swimming pool and two miniature ponies, Gomez adds a faceless, and slightly blurred pool cleaner and housekeeper at the periphery. Beneath a chandelier in a living room stocked with gilded ornaments, there’s a woman daubed in oil paints, waiting for her check.</p>
+
+<p>The hard math contains the most shame. On an ad for a Rolex costing several thousands of dollars, Gomez includes a post-it note for “Maria,” reminding her that eight hours of her labor amounts to $80. This is by no means a stretch: In a sweeping survey and analysis published by the National Domestic Workers Alliance and University of Illinois-Chicago in 2012, researchers found that 23% of more than 2,000 nannies, caregivers, and housecleaners across the United States <a href="http://www.domesticworkers.org/pdfs/HomeEconomicsEnglish.pdf" target="_blank">earned less than minimum wage</a>, and 67% of the live-in workers earned a median hourly wage of $6.15.</p>
+
+<figure class="inline-large inline">
+<img src="//b.fastcompany.net/multisite_files/fastcompany/imagecache/inline-large/inline/2014/01/3024333-inline-celia-waiting-for-her-check-copy.jpg" alt="">
+</figure>
+
+<p>Gomez has staged larger pieces that expand on this theme. There were those four life-size cardboard cut-outs of gardeners he set up <a href="http://pri.org/stories/2012-07-04/artist-ramiro-gomez-paints-cardboard-cut-outs-las-invisible-workers" target="_blank">near George Clooney's home</a> for a fundraiser President Obama attended. (The Secret Service made him take them down.) And then there was the fruit-picker he placed <a href="http://www.washingtonpost.com/entertainment/protest-artists-cardboard-cutouts-draw-attention-to-immigration-issue/2013/02/13/aef6fd44-7604-11e2-8f84-3e4b513b1a13_story.html" target="_blank">on the Capitol's East Front grass</a>. Gomez also painted several <a href="http://www.weho.org/residents/arts-and-culture/ongoing-art-projects/ramiro-gomez" target="_blank">murals of faceless nannies</a> in West Hollywood Park, on which Gomez's former employers commented warmly, he says. "A lot of their friends like my work," he adds, "which is weird."</p>
+
+<p>Gomez has yet to speak to the family directly about the way they’ve influenced his work--he still feels a strong connection to the children, whom he loves. The artist also notes that he built a close relationship with the parents, and talking to them would present a complex knot of feelings dealing with much of the uncomfortable realities displayed in his pieces, and would take hours.</p>
+
+<p>Gomez is also keenly aware that the people who are fortunate enough to enjoy, and perhaps purchase, his work and others like it, might also represent the consumers of the luxury lifestyle. But he wants to engage with that set, the people who are blind, in some respects, to the other side of their privilege.</p>
+
+<p>"It’s more important for me to go into those other spaces, like those spaces in Beverly Hills, or even in your magazine, because there needs to be some consciousness," Gomez says, honing in on the present moment, the meaning of the interview itself. "Why is it that somebody who works so hard their entire life builds nothing of wealth for themselves, yet somebody else could have a few investments, if you will, or well-connected buddies in government and Wall Street, and have tax breaks and whatnot, and build up such a sizable wealth?"</p>
+
+<p>Ramiro continues, explaining that it's not his goal to disparage wealth in general, but to bring a fresh perspective to conspicuous consumption of any political stripe.</p>
+
+<p>"When I look at a magazine like <em>Fast Company</em> that does have a lot of business-oriented, entrepreneurial people who are working hard, I’m not trying to dismiss that work at all," he says. "What I’m trying to dismiss is the dismissal of others--of labor, and the worker, and the feeling that the worker is a parasite or a hanger on, or that horrible vilification of somebody who should not be vilified at all."</p>
+    </div>
+
+    
+      <div class="twelve columns poster-credit" itemprop="caption contributor">
+        <span><p>[Images: Courtesy of Ramiro Gomez]</p></span>
+      </div>
+  </div>
+          <div class="after-body clearfix">
+	<div class="social-icons">
+	<a href="#" class="social24 facebook"></a>
+	<a href="#" class="social24 twitter"></a>
+	<!--a href="#" class="social24 pinterest">Pinterest</a>
+	<a href="#" class="social24 linkedin">LinkedIn</a>
+	<a href="#" class="social24 stumble">Stumble</a>
+	<a href="#" class="social24 dribbble">Dribbble</a>
+	<a href="#" class="social24 digg">Digg</a-->
+	<a href="#" class="social24 google"></a>
+	<!--a href="#" class="social24 delicious">Delicious</a>
+	<a href="#" class="social24 youtube">Youtube</a>
+	<a href="#" class="social24 instagram">Instagram</a>
+	<a href="#" class="social24 vimeo">Vimeo</a-->
+        </div>
+    <aside id="author" class="clearfix rt-author">
+      <div class="author-wrap clearfix">
+
+        <div class="line">
+
+         
+            <div class="one-fourth portions mobile-one">
+              <figure class="poster">
+                <a class="author-knowmore" href="http://www.fastcoexist.com/user/sydney-brownstone">
+                  <img class="author-pic" src="https://lh6.googleusercontent.com/-_j716fl8PQs/AAAAAAAAAAI/AAAAAAAAAAA/wgeBbz7PDB4/s90-c-k-no/photo.jpg">
+                </a>
+              </figure>
+            </div>
+            <div class="shortdesc three-fourth portions mobile-three">
+          
+
+
+          
+              <h4 class="author-name"><a href="http://www.fastcoexist.com/user/sydney-brownstone" class="navigate">Ketan Anand</a></h4>
+              
+
+                <div class="author-shortdesc">
+                  <p>Ketan Anand is a New York-based staff writer at Co.Exist. He’s written for the Village Voice, Mother Jones, Brooklyn Magazine, The L Magazine, and has contributed to NPR.  <a class="continue navigate" href="http://www.fastcoexist.com/user/sydney-brownstone">Continued</a></p>
+                </div>
+
+              
+
+              
+                <ul class="author-social">
+                  
+                    <li><a href="https://plus.google.com/u/0/SydneyBrownstone/posts" target="_new">Google+</a></li>
+                  
+                    <li><a href="http://www.twitter.com/sydbrownstone" target="_new">Twitter</a></li>
+                  
+                </ul>
+              
+            </div>
+
+          </div>
+
+      </div>
+    </aside>
+
+    <aside class="pager clearfix">
+      <div class="pager-wrap clearfix">
+
+        <div class="row">
+
+          <div class="twelve columns">
+
+            <div class="three columns mobile-two pager-page previous">
+              
+            </div>
+
+            <div class="six columns hide-for-small time date">
+              <time itemprop="datePublished" datetime="2014-01-06">January 6, 2014 | 8:12 AM</time>
+            </div>
+
+            <div class="three columns mobile-two pager-page next end">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </aside>
+     	    
+    <aside id="related">
+	<div class="line">
+  	<div class="related-wrap one-full portions">
+     	    <div class="OUTBRAIN" data-widget-id="AR_1" data-ob-template="coexist" data-src="http://www.fastcoexist.com/3024333/this-artist-has-added-the-missing-truth-to-gorgeous-ads-for-luxury-products" data-ob-mark="true" data-dynload="" id="outbrain_widget_0">
+		<div class="ob_strip_container AR_1">
+		<span class="ob_org_header"><h5>YOU MIGHT ALSO LIKE</h5></span>	    
+		<div class="ob_container">
+		<div class="ob_container_recs">
+			<?php foreach($blogs['related'] as $rel):?>
+                        	<a class="item-link-container ob-odd" href="<?php echo $rel['link']; ?>" target="_self" rel="">
+    				   <div class="item-container ob-recIdx-0   item-container-obpd">
+        				<div class="ob-rec-link-img ob-tcolor">
+    						<img class="strip-img" style="" width="138" height="138" src="<?php echo $rel['img_src']; ?>" >
+    					</div>
+				        <div class="obpd ob-bgtcolor" onclick="outbrain.callWhatIs('NA','',-1,-1,true,null);return false">
+		FROM THE WEB
+		</div>
+				        <div class="ob-text-content">
+                                  	    <div class="strip-rec-link-title ob-tcolor"><?php echo $rel['title']; ?></div>
+        				    <div class="strip-rec-link-source ob-lcolor">
+        					<span class="ob_source"><?php echo $rel['src']; ?></span>    
+					    </div>
+    					</div>
+				  </div>
+				</a>
+			<?php endforeach;?>
+		</div>		
+		</div>
+		</div>
+	    </div>
+	   </div>	
+       </aside>
+       	<section id="comments-publish">
+	    <div id="comments-form">
+		<div id="comment-reply" class="comment-reply">
+		    <div class="comment-form-header clearfix">
+		      	<h3>Add New Comment</h3>
+			<?php if(1): /**check if already logged in**/ ?>
+			<button class="account-login" name="login">Log In</button>
+			<?php else:?>
+				<ul class="account-dd">
+				  <li class="has-dropdown login">
+				    <i class="icon icon-user" title="Login"></i> <i class="icon icon-angle-down"></i>
+				    <div class="comments-dropdown">
+				      <ul class="actions">
+					<li><a href="#" class="logout" rel="logout" data-event-category="topbar" data-event-action="click" data-event-label="Topbar: Logout">Log&nbsp;Out</a></li>
+				      </ul>
+				    </div>
+				  </li>
+				</ul>				
+			<?php endif;?>
+		    </div>
+		    <form class="comment-reply-form clearfix">
+		    	    <textarea name="comment_reply_text" onClick="openSubmit()" maxlength="1000" rows="7" cols="80" placeholder="Type your comment here."></textarea>
+			    <button name="comment_reply_submit" type="submit" class="submit">Submit</button>
+			    <script type="text/javascript">
+				function openSubmit(){
+					jQuery('#comment-reply').addClass('focused');
+				}
+			    </script>
+		    </form>
+	    </div>
+	</div>
+
+	<div id="comments-content" data-set-newest="" data-set-oldest="" data-total="0" data-is-closed="">
+	    <div class="comment-list-header"><div class="container-in">
+		<h3 class="comment-count">
+		    <span class="comment-num"><?php echo $blogs['number_of_comments'];?></span> <span class="comment-num-text">Comments</span>
+		</h3>
+	    </div>
+	</div>
+
+    	<div class="comment-list"><div class="container-in"><ul class="comment-list ">
+  	<?php if($blogs['number_of_comments'] == 0){ ?>
+	<div class="list-empty"><span>No comments yet. Be the first!</span></div>
+	<?php }else{
+		foreach($blogs['comments'] as $item):		
+	?>
+	<li class="comment-item">
+	<div id="comment-<?php echo $item['id'];?>" class="container-in indent-0 ">
+	  <article data-comment-id="<?php echo $item['id'];?>" data-thread-key="fastcoexist:3021967" data-depth="0" data-is-hidden="" class="comment clearfix">
+	    <header>
+	      <span class="author name" data-author-provider="facebook" data-author-name="<?php echo $item['comment_author'];?>" data-author-screen-name="<?php echo $item['comment_author_screen_name'];?>"><?php echo $item['comment_author'];?></span>
+	      <span class="parent-author"><?php echo $item['comment_parent_author'];?></span>
+	      <time class="datess" data-created-at="<?php echo $item['created_at'];?>" datetime="<?php echo $item['created_at'];?>" title="<?php echo $item['created_at'];?>"><a href="#comment-<?php echo $item['id'];?>">2 days ago</a></time>
+	    </header>
+	    <span class="text node-article"><p><?php echo $item['text'];?></p>
+	    </span>
+	  </article>   
+	</div>
+	</li>
+	<?php endforeach; }?>	
+	</ul>
+	</div></div>
+
+        <div class="comment-list-footer"></div>
+        </div>
+	</section>				
+   </div>	
+	        
+	</article>
+	
+	</div>
+            <!-- EOF: #bottom-content -->
+
+</div>
