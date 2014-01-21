0654748d74e440e641ca507e936cb926680057eb
Saurabh <saurabh@saurabh.(none)>
2014-01-21 20:04:51 +0530

    added author file

---
+++ application/models/author.php
@@ -0,0 +1,20 @@
+<?php
+require_once(APPPATH . 'models/abstract.php');
+class Author extends AbstractModel{
+
+	private $_table = 'author';
+
+	public function loadById($id){
+		if ($id){
+			$query = $this->db->get_where($this->_table, array('author_id' => $id));
+			$result = $query->result_array();
+			if ($count = count($result)){
+				return $result;
+			}else{
+				return false;
+			}
+		}
+		return false;
+	}
+
+}
