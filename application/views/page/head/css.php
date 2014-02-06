97a70ce1104daa85f3a8f165a7fef3a8e4a86375
Saurabh <saurabh@saurabh.(none)>
2014-02-06 13:32:02 +0530

    added relative paths

--- application/views/page/head/css.php
+++ application/views/page/head/css.php
@@ -1,33 +1,33 @@
 <style type="text/css" media="all">
-@import url("http://www.recordstree.com:8081/public/css/core/system.base.css");
-@import url("http://www.recordstree.com:8081/public/css/core/system.menus.css");
-@import url("http://www.recordstree.com:8081/public/css/core/system.messages.css");
-@import url("http://www.recordstree.com:8081/public/css/core/system.theme.css");
+@import url("/public/css/core/system.base.css");
+@import url("/public/css/core/system.menus.css");
+@import url("/public/css/core/system.messages.css");
+@import url("/public/css/core/system.theme.css");
 </style>
 <style type="text/css" media="all">
-@import url("http://www.recordstree.com:8081/modules/comment/comment.css");
-@import url("http://www.recordstree.com:8081/modules/field/theme/field.css");
-@import url("http://www.recordstree.com:8081/modules/node/node.css");
-@import url("http://www.recordstree.com:8081/modules/search/search.css");
-@import url("http://www.recordstree.com:8081/modules/user/user.css");
+@import url("/modules/comment/comment.css");
+@import url("/modules/field/theme/field.css");
+@import url("/modules/node/node.css");
+@import url("/modules/search/search.css");
+@import url("/modules/user/user.css");
 </style>
 <style type="text/css" media="all">
-@import url("http://www.recordstree.com:8081/public/css/plugins/prettyPhoto.css");
-@import url("http://www.recordstree.com:8081/public/css/shortcodes/buttons.css");
-@import url("http://www.recordstree.com:8081/public/css/plugins/fancybox.css");
+@import url("/public/css/plugins/prettyPhoto.css");
+@import url("/public/css/shortcodes/buttons.css");
+@import url("/public/css/plugins/fancybox.css");
 </style>
 <style type="text/css" media="all">
-@import url("http://www.recordstree.com:8081/public/css/main-css.css");
-@import url("http://www.recordstree.com:8081/public/css/normalize.css");
-@import url("http://www.recordstree.com:8081/public/css/plugins/flexslider.css");
-@import url("http://www.recordstree.com:8081/public/css/local.css");
+@import url("/public/css/main-css.css");
+@import url("/public/css/normalize.css");
+@import url("/public/css/plugins/flexslider.css");
+@import url("/public/css/local.css");
 </style>
 <style type="text/css" media="all and (min-width: 768px) and (max-width: 959px)">
-@import url("http://www.recordstree.com:8081/public/css/768.css");
+@import url("/public/css/768.css");
 </style>
 <style type="text/css" media="all and (min-width: 480px) and (max-width: 767px)">
-@import url("http://www.recordstree.com:8081/public/css/480.css");
+@import url("/public/css/480.css");
 </style>
 <style type="text/css" media="all and (max-width: 479px)">
-@import url("http://www.recordstree.com:8081/public/css/320.css");
+@import url("/public/css/320.css");
 </style>
